74cfd42f3e7bbc699017cbe25835baa7189e6efd
sharteeya <abcdok77@gmail.com>
2021-10-22 22:50:29 +0800

    change tab to spaces

--- drawuser.py
+++ drawuser.py
@@ -10,63 +10,63 @@ GREEN = "\033[0;32m"
 NC = "\033[0m"
 
 if __name__ == "__main__":
-	"""
-	Get all student accounts and all student accounts that are currently login.
-	"""
-	all_users = [
-			user
-			for user
-			in os.listdir("/home")
-			if str(user).startswith("a") # Edit this for filtering student account.
-			or str(user).startswith("m") # Edit this for filtering student account.
-		]
-	current_users = list(set(
-			user.name
-			for user
-			in psutil.users()
-			if str(user.name).startswith("a") # Edit this for filtering student account.
-			or str(user.name).startswith("m") # Edit this for filtering student account.
-		))
-	random.shuffle(all_users)
-	
-	"""
-	Get the input number and inspect if it is a number.
-	"""
-	if len(sys.argv) < 2:
-		draw_number = 0
-		print(f"{WHITE}Invalid input. "
-			  f"Use {YELLOW}draw <number> {WHITE}to draw <number> students.{NC}"
-		)
-		exit()
-	else:
-		try:
-			draw_number = int(sys.argv[1])
-		except:
-			print(f"{WHITE}Invalid input.{NC}")
-			exit()
+    """
+    Get all student accounts and all student accounts that are currently login.
+    """
+    all_users = [
+            user
+            for user
+            in os.listdir("/home")
+            if str(user).startswith("a") # Edit this for filtering student account.
+            or str(user).startswith("m") # Edit this for filtering student account.
+        ]
+    current_users = list(set(
+            user.name
+            for user
+            in psutil.users()
+            if str(user.name).startswith("a") # Edit this for filtering student account.
+            or str(user.name).startswith("m") # Edit this for filtering student account.
+        ))
+    random.shuffle(all_users)
+    
+    """
+    Get the input number and inspect if it is a number.
+    """
+    if len(sys.argv) < 2:
+        draw_number = 0
+        print(f"{WHITE}Invalid input. "
+              f"Use {YELLOW}draw <number> {WHITE}to draw <number> students.{NC}"
+        )
+        exit()
+    else:
+        try:
+            draw_number = int(sys.argv[1])
+        except:
+            print(f"{WHITE}Invalid input.{NC}")
+            exit()
 
-	"""
-	If the input number is larger than the total number of students,
-	set it to the max student number.
-	"""
-	if draw_number > len(all_users):
-		draw_number = len(all_users)
-	elif draw_number <= 0:
-		print(f"{WHITE}Invalid input. The number should be larger than zero.{NC}")
-		exit()
+    """
+    If the input number is larger than the total number of students,
+    set it to the max student number.
+    """
+    if draw_number > len(all_users):
+        draw_number = len(all_users)
+    elif draw_number <= 0:
+        print(f"{WHITE}Invalid input. The number should be larger than zero.{NC}")
+        exit()
 
-	"""
-	Draw students.
-	"""
-	print(
-			f"{YELLOW}Draw {draw_number} student(s):\n"
-			f"{NC}=============================="
-	)
-	for idx in range(draw_number):
-		print(f"{WHITE}No.{idx+1}:\t{all_users[idx].upper()}    ", end="")
-		if all_users[idx] in current_users:
-			print(f"{GREEN}ONLINE")
-		else:
-			print(f"{RED}OFFLINE")
+    """
+    Draw students.
+    """
+    print(
+            f"{YELLOW}Draw {draw_number} student(s):\n"
+            f"{NC}=============================="
+    )
+    for idx in range(draw_number):
+        print(f"{WHITE}No.{idx+1}:\t{all_users[idx].upper()}    ", end="")
+        if all_users[idx] in current_users:
+            print(f"{GREEN}ONLINE")
+        else:
+            print(f"{RED}OFFLINE")
 
-	print(f"{NC}")
+    print(f"{NC}")
